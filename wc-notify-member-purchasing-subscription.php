978223f4d20899bb3c967665c0d096288c1685ab
Joey <joey@pie.co.de>
2022-04-12 15:44:37 +0100

    init

---
+++ wc-notify-member-purchasing-subscription.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: WC Add Notice of Second Subscription
+Description: Adds a notice when validating a WooCommerce cart if an active member tries to purchase a subscription
+Version:     0.1
+Author:      The team at PIE
+Author URI:  http://pie.co.de
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+/* WooCommerce/Stripe Add New Card as Default is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+
+WooCommerce/Stripe Add New Card as Default is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with WooCommerce/Stripe Add New Card as Default. If not, see https://www.gnu.org/licenses/gpl-3.0.en.html */
+
+namespace PIE\WCNotifyMemberPurchasingSubscription;
+
+if ( function_exists( 'wc_memberships_is_user_active_member' ) ) {
+  add_filter( 'woocommerce_add_to_cart_validation', 'add_purchase_sub_notice', 20, 3 );
+  function add_purchase_sub_notice( $valid, $product_id, $quantity ) {
+      $user = get_current_user_id();
+      if ( $user && wc_memberships_is_user_active_member( $user ) ) {
+          wc_add_notice( sprintf( __( 'You already have an active subscription, are you sure you want to create another one? %sView your subscriptions.%s' ), '<a href="' . wc_get_page_permalink( "myaccount" ) . 'subscriptions">', '</a>' ), 'notice' );
+      }
+      return $valid;
+  }
+}
